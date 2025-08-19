7a458c004737aa1a21046ca7babc51e4d523a071
dimitri.anoudis <dimitri.anoudis@news.co.uk>
2025-08-19 04:11:01 +0300

    ci: GitHub Actions with OIDC; build & push backend image; terraform apply; build & deploy frontend

---
+++ infra/github_oidc.tf
@@ -0,0 +1,102 @@
+# GitHub OIDC provider (if not already present in your account)
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+
+  tags = local.tags
+}
+
+data "aws_iam_policy_document" "gha_assume" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:*/*:ref:refs/heads/main"]
+    }
+  }
+}
+
+resource "aws_iam_role" "gha_deploy" {
+  name               = "${var.project}-gha-deploy"
+  assume_role_policy = data.aws_iam_policy_document.gha_assume.json
+  tags               = local.tags
+}
+
+# Narrow permissions used by our workflow
+data "aws_iam_policy_document" "gha_policy" {
+  statement {
+    sid     = "ECRPush"
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:CompleteLayerUpload",
+      "ecr:InitiateLayerUpload",
+      "ecr:PutImage",
+      "ecr:UploadLayerPart",
+      "ecr:DescribeRepositories",
+      "ecr:BatchGetImage",
+      "ecr:CreateRepository"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid = "S3Upload"
+    actions = [
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+      "s3:DeleteObject"
+    ]
+    resources = ["${aws_s3_bucket.frontend.arn}/*"]
+  }
+
+  statement {
+    sid       = "CFInvalidate"
+    actions   = ["cloudfront:CreateInvalidation"]
+    resources = [aws_cloudfront_distribution.site.arn]
+  }
+
+  statement {
+    sid = "TerraformInfra"
+    actions = [
+      "apigateway:*",
+      "apigatewayv2:*",
+      "lambda:*",
+      "iam:*",
+      "cloudwatch:*",
+      "logs:*",
+      "ecr:*",
+      "dynamodb:*",
+      "s3:*",
+      "cloudfront:*",
+      "sts:*"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "gha_policy" {
+  name   = "${var.project}-gha-policy"
+  policy = data.aws_iam_policy_document.gha_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "gha_attach" {
+  role       = aws_iam_role.gha_deploy.name
+  policy_arn = aws_iam_policy.gha_policy.arn
+}
+
+output "gha_role_arn" {
+  value = aws_iam_role.gha_deploy.arn
+}
